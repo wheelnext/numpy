dfb0163a30a056dc9c1d650464eb51f49624efd4
Michał Górny <mgorny@gentoo.org>
2025-03-20 17:14:02 +0100

    Squashed wheel variant support
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

---
+++ build-variants.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -ex
+
+pip config set global.extra-index-url https://pypi.anaconda.org/mgorny/simple
+
+export INSTALL_OPENBLAS=false
+LABEL=${BLAS:-openblas}
+case ${BLAS:-openblas} in
+	openblas)
+		export INSTALL_OPENBLAS=true
+		;;
+	mkl)
+		pip install mkl-devel
+		export LDFLAGS="$LDFLAGS -Wl,-rpath,\$ORIGIN/../../../.."
+		;;
+esac
+
+if [ -n "${X8664}" ]; then
+	LABEL=x8664v4_${LABEL}
+	set -- "${@}" "-Cvariant=x86_64::level::${X8664}"
+fi
+
+set -- "${@}" -Cvariant-label=${LABEL}
+
+. tools/wheels/cibw_before_build.sh "${PWD}"
+export PKG_CONFIG_PATH
+pip install build auditwheel delocate
+python -m build -w "${@}"
+mkdir wheelhouse
+
+# tag updating needs to be updated for variants
+set -- dist/*.whl
+old_name=${1#*/}
+# strip variant label to avoid issues with auditwheel
+mv "${1}" "${1%-*}.whl"
+
+if grep -q Ubuntu /etc/os-release; then
+	auditwheel repair --exclude 'libmkl*' --exclude 'libiomp*' dist/*.whl
+	mv wheelhouse/*.whl "wheelhouse/${old_name}"
+else
+	delocate-wheel -w wheelhouse dist/*.whl
+	mv wheelhouse/*.whl "wheelhouse/${old_name}"
+fi
