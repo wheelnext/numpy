7cc2d03052057863b28daa875aaa53f4edfec9d1
Michał Górny <mgorny@gentoo.org>
2025-03-20 17:14:02 +0100

    Squashed wheel variant support
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

---
+++ build-variants.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -ex
+
+export INSTALL_OPENBLAS=false
+case ${BLAS:-openblas} in
+	openblas)
+		export INSTALL_OPENBLAS=true
+		;;
+	mkl)
+		pip install mkl-devel
+		export LDFLAGS="$LDFLAGS -Wl,-rpath,\$ORIGIN/../../../.."
+		;;
+esac
+
+set -- "${@}" -Cvariant-label=${BLAS}
+
+. tools/wheels/cibw_before_build.sh "${PWD}"
+export PKG_CONFIG_PATH
+pip install build auditwheel delocate
+python -m build -w "${@}"
+mkdir wheelhouse
+
+# tag updating needs to be updated for variants
+set -- dist/*.whl
+old_name=${1#*/}
+# strip variant label to avoid issues with auditwheel
+mv "${1}" "${1%-*}.whl"
+
+if grep -q Ubuntu /etc/os-release; then
+	auditwheel repair --exclude 'libmkl*' dist/*.whl
+	mv wheelhouse/*.whl "wheelhouse/${old_name}"
+else
+	delocate-wheel -w wheelhouse dist/*.whl
+	mv wheelhouse/*.whl "wheelhouse/${old_name}"
+fi
