f67310453f81225cc82006c89e54d5e55030e38d
Michał Górny <mgorny@quansight.com>
2025-05-09 18:45:12 +0200

    fix, add arm64 job

--- build-variants.sh
+++ build-variants.sh
@@ -2,7 +2,7 @@
 
 . tools/wheels/cibw_before_build.sh "${PWD}"
 export PKG_CONFIG_PATH
-pip install build
+pip install build auditwheel
 python -m build -w "${@}"
 # tag updating needs to be updated for variants
 auditwheel repair --no-update-tags --plat manylinux_2_34_x86_64 dist/*.whl
