8ec262e905f19f015cfa63318b6c88087ea01320
Michał Górny <mgorny@quansight.com>
2025-05-09 19:19:44 +0200

    try again

--- build-variants.sh
+++ build-variants.sh
@@ -11,4 +11,7 @@ python -m build -w "${@}"
 if grep -q Ubuntu /etc/os-release; then
 	# tag updating needs to be updated for variants
 	auditwheel repair --no-update-tags --plat "$PLAT" dist/*.whl
+else
+	# quick hack to make workflows simpler
+	mv dist wheelhouse
 fi
