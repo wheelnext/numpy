bfb918eaf7df344af7c4cb9414f8fdf979651185
Michał Górny <mgorny@quansight.com>
2025-05-09 19:15:39 +0200

    try fixing macos

--- build-variants.sh
+++ build-variants.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+if ! grep -q Ubuntu /etc/os-release; then
+	export INSTALL_OPENBLAS=false
+fi
+
 . tools/wheels/cibw_before_build.sh "${PWD}"
 export PKG_CONFIG_PATH
 pip install build auditwheel
 python -m build -w "${@}"
-# tag updating needs to be updated for variants
-auditwheel repair --no-update-tags --plat "$PLAT" dist/*.whl
+if grep -q Ubuntu /etc/os-release; then
+	# tag updating needs to be updated for variants
+	auditwheel repair --no-update-tags --plat "$PLAT" dist/*.whl
+fi
