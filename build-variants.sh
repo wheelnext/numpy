a52692633bc205340c5aab043e7661ec74962059
Michał Górny <mgorny@quansight.com>
2025-06-30 20:43:23 +0200

    Fix auditwheel

--- build-variants.sh
+++ build-variants.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 if ! grep -q Ubuntu /etc/os-release; then
 	export INSTALL_OPENBLAS=false
 fi
@@ -10,7 +12,12 @@ pip install build auditwheel
 python -m build -w "${@}"
 if grep -q Ubuntu /etc/os-release; then
 	# tag updating needs to be updated for variants
-	auditwheel repair --no-update-tags --plat auto dist/*.whl
+	set -- dist/*.whl
+	old_name=${1#*/}
+	# strip variant label to avoid issues with auditwheel
+	mv "${1}" "${1%-*}.whl"
+	auditwheel repair dist/*.whl
+	mv wheelhouse/*.whl "wheelhouse/${old_name}"
 else
 	# quick hack to make workflows simpler
 	mv dist wheelhouse
