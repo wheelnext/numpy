b5bcae841f7504e304f54a8d7b078def64733c2e
Michał Górny <mgorny@gentoo.org>
2026-09-10 17:33:30 +0200

    Update project metadata
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- build-variants.sh
+++ build-variants.sh
@@ -24,7 +24,7 @@ fi
 set -- "${@}" -Cvariant-label=${LABEL}
 
 . tools/wheels/cibw_before_build.sh "${PWD}"
-export PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=${pkgconf_path}
 pip install build auditwheel delocate
 python -m build -w "${@}"
 mkdir wheelhouse
